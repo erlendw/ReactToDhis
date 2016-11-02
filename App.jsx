cda7a6c3b27491baf31198f5f4fcff36599c654a
erlendw <erlendwestbye@gmail.com>
2016-11-02 12:43:29 +0100

    initial commit fool

---
+++ App.jsx
@@ -0,0 +1,89 @@
+import React from 'react';
+
+import { Button } from 'react-bootstrap';
+import { CreateStore } from 'redux';
+import Request from 'superagent'
+
+class App extends React.Component {
+
+    constructor() { //class constructor
+        super();
+        this.state = {
+            svampebober: "jeg er svampebob"
+        };
+
+    }
+
+    componentWillMount() {
+
+        var url = 'https://chat.berg-hansen.no/I3Root/websvcs/chat/start'
+        Request
+            .post(url).set("application/x-www-form-urlencoded").send({})
+            .end(function (err, res) {
+                if (err || !res.ok) {
+                    alert('Oh no! error');
+                } else {
+                    alert('yay got ' + JSON.stringify(res.body));
+                }
+            });
+
+        /*.then((response) => {
+
+                //this.setState({svampebober : response})
+                console.log(response)
+
+            })*/
+        //console.log("lastet")    
+
+    }
+
+    clicked(innnn) {
+        console.log("was clicked son")
+
+        this.setState({ svampebober: innnn });
+
+    }
+
+    render() {
+        return (
+            <div className="container-fluid">
+                {this.state.svampebober}
+                <Button bsStyle="primary" bsSize="large" onClick={(e) => { this.clicked("jeg er ikke svampebob") } } >Large button</Button>
+
+            </div>
+        );
+    }
+}
+
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Header</h1>
+            </div>
+        );
+    }
+}
+
+class Content extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Content</h2>
+                <p>The content text!!!</p>
+            </div>
+        );
+    }
+}
+
+class Footer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>I am the footer</h2>
+            </div>
+        );
+    }
+}
+
+export default App;
