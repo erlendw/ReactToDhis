d914efc0002d0f65853f0e5798b6821e2f3b731d
erlendw <erlendwestbye@gmail.com>
2016-11-29 13:47:53 +0100

    Scaffolding for et form per level

---
+++ containers/forms/facilityForm.js
@@ -0,0 +1,46 @@
+/**
+ * Created by erlend on 29.11.2016.
+ */
+
+
+import React from 'react';
+import {connect} from 'react-redux';
+
+import {showChangeOrgModal, addNewOganisationUnit} from '../../actions/actions'
+
+
+
+import { Button,Table,Navbar,Nav,NavItem,Form,ControlLabel, NavDropdown, FormGroup,FormControl, NavbarBrand, MenuItem, DropdownButton, Well, Panel, ButtonGroup, hr} from 'react-bootstrap';
+import ReactDOM from 'react-dom';
+import {bindActionCreators} from 'redux'
+
+class facilityForm extends React.Component {
+
+    render(){
+
+        return(
+            <div>Hella world</div>
+        )
+    }
+
+};
+
+
+const mapStateToProps = (state) => {
+    return{
+        changeOrg : state.changeOrg,
+        currentOrg: state.currentOrg
+    }
+
+};
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+
+        showChangeOrgModal : (b) => {dispatch(showChangeOrgModal(b))},
+        addNewOganisationUnit: (name, shortName, openingDate) => dispatch(addNewOganisationUnit(name, shortName, openingDate))
+
+    }
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(facilityForm);
